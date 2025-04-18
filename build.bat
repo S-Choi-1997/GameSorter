eac6f6671b2fe3dc5ad1dc46ee4bb8b74c3ebab0
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2025-04-18 13:21:03 +0900

    init branch

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd d EProjectsgame-sort
-pyinstaller --onefile --windowed --name=방주이름생성기 --icon=app.ico --hidden-import=PySide6 --hidden-import=openai --hidden-import=requests --hidden-import=bs4 gamesort.py
-pause
